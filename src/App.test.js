acd1eda60cd84b300dc79812a291b77ca448fd61
Alexander Ponomarenko <aaponomarenko@vtb.ru>
2023-05-25 12:14:30 +0300

    Добавлены тесты для fetchBooksWorker - testSaga, expectSaga

--- src/App.test.js
+++ src/App.test.js
@@ -2,13 +2,13 @@ import { render, screen } from '@testing-library/react'
 import App from './App'
 import { Provider } from 'react-redux'
 import { store } from './store'
-//
-// it('renders "Book Author" title', () => {
-//     render(
-//         <Provider store={store}>
-//             <App />
-//         </Provider>
-//     )
-//     const caption = screen.getByText(/Book Author/i)
-//     expect(caption).toBeInTheDocument()
-// })
+
+xit('renders "Book Author" title', () => {
+    render(
+        <Provider store={store}>
+            <App />
+        </Provider>
+    )
+    const caption = screen.getByText(/Book Author/i)
+    expect(caption).toBeInTheDocument()
+})
--- src/sagas/books.test.js
+++ src/sagas/books.test.js
@@ -1,5 +1,5 @@
 import { call, put, take, race, delay } from 'redux-saga/effects'
-
+import { testSaga, expectSaga } from 'redux-saga-test-plan'
 import { fetchBooksWorker } from './books'
 import { actions, ActionTypes } from '../actions/common'
 import SERVER from '../actions/server'
@@ -27,10 +27,78 @@ describe('fetchBooksWorker', () => {
 
         expect(gen.next().done).toEqual(true)
     })
-    it('should cancel books request', function () {
 
+    it('should fetch books using testSaga', function () {
+        const response = { data: [] }
+
+        testSaga(fetchBooksWorker)
+            .next()
+            .put(actions.fetchingBooks(true))
+            .next()
+            .race([
+                call(SERVER.get, '/books'),
+                delay(12000, true),
+                take(ActionTypes.CANCEL_FETCH_BOOK)
+            ])
+            .next([response])
+            .put(actions.fetchBooksFulFilled(response.data))
+            .next()
+            .put(actions.fetchingBooks(false))
+            .next()
+            .isDone()
+    })
+
+    it('should fetch books using expectSaga', function () {
+        const response = { data: [] }
+
+        return expectSaga(fetchBooksWorker)
+            .provide([
+
+                [race([
+                    call(SERVER.get, '/books'),
+                    delay(12000, true),
+                    take(ActionTypes.CANCEL_FETCH_BOOK)
+                ]), [response]]
+
+            ])
+            .put(actions.fetchBooksFulFilled(response.data))
+            .run()
+    })
+
+    it('should cancel books request', function () {
+        const response = { data: [] }
+
+        return expectSaga(fetchBooksWorker)
+            .provide([
+
+                [race([
+                    call(SERVER.get, '/books'),
+                    delay(12000, true),
+                    take(ActionTypes.CANCEL_FETCH_BOOK)
+                ]), [undefined, undefined, true]]
+
+            ])
+            .put(actions.fetchingBooks(false))
+            .not.put(actions.fetchBooksFulFilled(response.data))
+            .not.put(actions.requestIsTimedOut(true))
+            .run()
     })
     it('should stop request by timeout', function () {
+        const response = { data: [] }
 
+        return expectSaga(fetchBooksWorker)
+            .provide([
+
+                [race([
+                    call(SERVER.get, '/books'),
+                    delay(12000, true),
+                    take(ActionTypes.CANCEL_FETCH_BOOK)
+                ]), [undefined, true, undefined]]
+
+            ])
+            .put(actions.fetchingBooks(false))
+            .not.put(actions.fetchBooksFulFilled(response.data))
+            .put(actions.requestIsTimedOut(true))
+            .run()
     })
 })
